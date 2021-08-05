2f47033991e7a83bbe6e990a454da44c4f0208eb
tabokie <xy.tao@outlook.com>
2021-08-06 00:02:39 +0800

    keep log items private
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- src/file_pipe_log.rs
+++ src/file_pipe_log.rs
@@ -18,7 +18,7 @@ use protobuf::Message;
 use crate::codec::{self, NumberEncoder};
 use crate::config::{Config, RecoveryMode};
 use crate::event_listener::EventListener;
-use crate::log_batch::{EntryExt, LogBatch, LogItemContent};
+use crate::log_batch::{EntryExt, LogBatch};
 use crate::pipe_log::{FileId, LogQueue, PipeLog};
 use crate::util::HandyRwLock;
 use crate::{Error, Result};
@@ -530,11 +530,7 @@ impl PipeLog for FilePipeLog {
                 }
             };
             if queue == LogQueue::Rewrite {
-                for item in log_batch.items.iter_mut() {
-                    if let LogItemContent::Entries(entries) = &mut item.content {
-                        entries.set_queue(LogQueue::Rewrite);
-                    }
-                }
+                log_batch.set_queue(LogQueue::Rewrite);
             }
             batches.push(log_batch);
             offset = (buf.as_ptr() as usize - start_ptr as usize) as u64;
@@ -553,11 +549,7 @@ impl PipeLog for FilePipeLog {
                 fd.sync()?;
             }
 
-            for item in batch.items.iter_mut() {
-                if let LogItemContent::Entries(entries) = &mut item.content {
-                    entries.set_position(queue, file_id, offset);
-                }
-            }
+            batch.set_position(queue, file_id, offset);
 
             return Ok((file_id, content.len()));
         }
