98069b351c1673507dce64551c862eb8fea1a819
tabokie <xy.tao@outlook.com>
2021-10-14 14:42:43 +0800

    fix deadlock and strengthen tests
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- src/write_barrier.rs
+++ src/write_barrier.rs
@@ -101,11 +101,13 @@ impl<'a, 'b, 'c, P, O> Iterator for WriterIter<'a, 'b, 'c, P, O> {
 pub struct WriteBarrier<P, O> {
     mutex: Mutex<()>,
     leader_cv: Condvar,
-    follower_cv: Condvar,
+    follower_cvs: [Condvar; 2],
 
     head: Cell<Ptr<Writer<P, O>>>,
     tail: Cell<Ptr<Writer<P, O>>>,
-    leader: Cell<Ptr<Writer<P, O>>>,
+
+    pending_leader: Cell<Ptr<Writer<P, O>>>,
+    pending_index: Cell<usize>,
 }
 
 unsafe impl<P: Send, O: Send> Send for WriteBarrier<P, O> {}
@@ -116,10 +118,11 @@ impl<P, O> Default for WriteBarrier<P, O> {
         WriteBarrier {
             mutex: Mutex::new(()),
             leader_cv: Condvar::new(),
-            follower_cv: Condvar::new(),
+            follower_cvs: [Condvar::new(), Condvar::new()],
             head: Cell::new(None),
             tail: Cell::new(None),
-            leader: Cell::new(None),
+            pending_leader: Cell::new(None),
+            pending_index: Cell::new(0),
         }
     }
 }
@@ -135,17 +138,27 @@ impl<P, O> WriteBarrier<P, O> {
             unsafe {
                 tail.as_ref().set_next(node);
             }
-            if self.leader.get().is_none() {
-                self.leader.set(node);
-                self.leader_cv.wait(&mut lk);
-            } else {
-                self.follower_cv.wait(&mut lk);
+            self.tail.set(node);
+
+            if self.pending_leader.get().is_some() {
+                // follower of next write group.
+                self.follower_cvs[self.pending_index.get() % 2].wait(&mut lk);
                 return None;
+            } else {
+                // leader of next write group.
+                self.pending_leader.set(node);
+                self.pending_index
+                    .set(self.pending_index.get().wrapping_add(1));
+                //
+                self.leader_cv.wait(&mut lk);
+                self.pending_leader.set(None);
             }
         } else {
+            // leader of a empty write group. proceed directly.
+            debug_assert!(self.pending_leader.get().is_none());
             self.head.set(node);
+            self.tail.set(node);
         }
-        self.tail.set(node);
 
         Some(WriteGroup {
             start: node,
@@ -159,14 +172,18 @@ impl<P, O> WriteBarrier<P, O> {
     /// responsible writers, and next write group should be formed.
     fn leader_exit(&self) {
         let _lk = self.mutex.lock();
-        if let Some(leader) = self.leader.get() {
-            self.head.set(Some(leader));
+        if let Some(leader) = self.pending_leader.get() {
+            // wake up leader of next write group.
             self.leader_cv.notify_one();
+            // wake up follower of current write group.
+            self.follower_cvs[self.pending_index.get().wrapping_sub(1) % 2].notify_all();
+            self.head.set(Some(leader));
         } else {
+            // wake up follower of current write group.
+            self.follower_cvs[self.pending_index.get() % 2].notify_all();
             self.head.set(None);
             self.tail.set(None);
         }
-        self.follower_cv.notify_all();
     }
 }
 
@@ -174,6 +191,7 @@ impl<P, O> WriteBarrier<P, O> {
 mod tests {
     use super::*;
     use std::sync::atomic::{AtomicU32, Ordering};
+    use std::sync::mpsc;
     use std::sync::Arc;
     use std::thread::Builder as ThreadBuilder;
     use std::time::Duration;
@@ -219,44 +237,84 @@ mod tests {
 
     #[test]
     fn test_parallel_groups() {
-        let barrier: WriteBarrier<(), u32> = Default::default();
+        let barrier: WriteBarrier<u32, u32> = Default::default();
         let barrier = Arc::new(barrier);
         let stats = Arc::new(WriterStats::new());
 
-        let payload = ();
-        let mut writer = Writer::new(&payload, false);
-        let wg = barrier.enter(&mut writer).unwrap();
+        let wid = 0;
+        let (tx, rx) = mpsc::sync_channel(0);
+
+        let mut writer = Writer::new(&wid, false);
+        let mut wg = barrier.enter(&mut writer).unwrap();
+        for w in wg.iter_mut() {
+            w.set_output(7);
+        }
 
         let mut ths = vec![];
-        for _ in 0..2 {
-            let (barrier_clone, stats_clone) = (barrier.clone(), stats.clone());
+        for i in 0..2 {
+            let (barrier_clone, stats_clone, tx_clone) =
+                (barrier.clone(), stats.clone(), tx.clone());
             ths.push(
                 ThreadBuilder::new()
                     .spawn(move || {
-                        let payload = ();
-                        let mut writer = Writer::new(&payload, false);
-                        if let Some(_wg) = barrier_clone.enter(&mut writer) {
+                        let wid = i + 1;
+                        let mut writer = Writer::new(&wid, false);
+                        if let Some(mut wg) = barrier_clone.enter(&mut writer) {
+                            for w in wg.iter_mut() {
+                                w.set_output(7);
+                            }
                             stats_clone.leader.fetch_add(1, Ordering::Relaxed);
+                            tx_clone.send(()).unwrap();
                         }
+                        writer.finish();
                         stats_clone.exited.fetch_add(1, Ordering::Relaxed);
                     })
                     .unwrap(),
             );
         }
 
-        std::thread::sleep(Duration::from_millis(1));
+        std::thread::sleep(Duration::from_millis(5));
         assert_eq!(stats.leader.load(Ordering::Relaxed), 0);
         assert_eq!(stats.exited.load(Ordering::Relaxed), 0);
         std::mem::drop(wg);
+        writer.finish();
 
-        let remaining_writers = ths.len();
+        std::thread::sleep(Duration::from_millis(1));
+        assert_eq!(stats.leader.load(Ordering::Relaxed), 1);
+        assert_eq!(stats.exited.load(Ordering::Relaxed), 0);
+
+        for i in 0..2 {
+            let (barrier_clone, stats_clone, tx_clone) =
+                (barrier.clone(), stats.clone(), tx.clone());
+            ths.push(
+                ThreadBuilder::new()
+                    .spawn(move || {
+                        let wid = i + 3;
+                        let mut writer = Writer::new(&wid, false);
+                        if let Some(mut wg) = barrier_clone.enter(&mut writer) {
+                            for w in wg.iter_mut() {
+                                w.set_output(7);
+                            }
+                            stats_clone.leader.fetch_add(1, Ordering::Relaxed);
+                            tx_clone.send(()).unwrap();
+                        }
+                        writer.finish();
+                        stats_clone.exited.fetch_add(1, Ordering::Relaxed);
+                    })
+                    .unwrap(),
+            );
+            std::thread::sleep(Duration::from_millis(1));
+            assert_eq!(stats.leader.load(Ordering::Relaxed), 1 + i);
+            rx.recv().unwrap();
+            std::thread::sleep(Duration::from_millis(1));
+            assert_eq!(stats.leader.load(Ordering::Relaxed), 2 + i);
+        }
+
+        rx.recv().unwrap();
         for th in ths.drain(..) {
             th.join().unwrap();
         }
-        assert_eq!(stats.leader.load(Ordering::Relaxed), 1);
-        assert_eq!(
-            stats.exited.load(Ordering::Relaxed),
-            remaining_writers as u32
-        );
+        assert_eq!(stats.leader.load(Ordering::Relaxed), 1 + 2);
+        assert_eq!(stats.exited.load(Ordering::Relaxed), 2 + 2);
     }
 }
