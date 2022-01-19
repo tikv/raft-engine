10cc8209cb8fd7ccdb2783d5e5ddd991841cf281
tabokie <xy.tao@outlook.com>
2022-01-19 21:41:22 +0800

    adapt to older toolchain
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(shrink_to)]
 #![feature(btree_drain_filter)]
 #![feature(generic_associated_types)]
 #![feature(test)]
