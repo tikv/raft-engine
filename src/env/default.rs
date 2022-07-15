c630979dca86a65d93854158b63492d08aa9d1a5
tabokie <xy.tao@outlook.com>
2022-07-15 12:38:14 +0800

    reseek after write failure
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- src/env/default.rs
+++ src/env/default.rs
@@ -232,6 +232,9 @@ impl Read for LogFile {
 
 impl Seek for LogFile {
     fn seek(&mut self, pos: SeekFrom) -> IoResult<u64> {
+        fail_point!("log_file::seek::err", |_| {
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "fp"))
+        });
         match pos {
             SeekFrom::Start(offset) => self.offset = offset as usize,
             SeekFrom::Current(i) => self.offset = (self.offset as i64 + i) as usize,
